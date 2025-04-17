5a0403492eee6efcec55dc058fb2d8892e65be94
cyan-sp <cyan.mv@gmail.com>
2025-04-16 19:38:48 -0600

    common and navbar components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AerolineasComponent } from './components/aerolineas/aerolineas.component';
+import { CommonComponent } from './components/common/common.component';
 import { NavbarComponent } from './components/common/navbar/navbar.component';
+// import { NavbarComponent } from './components/common/navbar/navbar.component';
 // import { NavbarComponent } from './components/common/navbar/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AerolineasComponent,
+    CommonComponent,
     NavbarComponent,
+    // NavbarComponent,
     // NavbarComponent
   ],
   imports: [
---
+++ src/app/components/common/common.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-common',
+  standalone: false,
+  templateUrl: './common.component.html',
+  styleUrl: './common.component.css'
+})
+export class CommonComponent {
+
+}
