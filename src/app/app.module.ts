4349a89c25c964b8c4c388be1535181fc7b2f0a3
cyan-sp <cyan.mv@gmail.com>
2025-04-16 18:33:47 -0600

    Navbar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AerolineasComponent } from './components/aerolineas/aerolineas.component';
+import { NavbarComponent } from './components/common/navbar/navbar.component';
 // import { NavbarComponent } from './components/common/navbar/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AerolineasComponent,
+    NavbarComponent,
     // NavbarComponent
   ],
   imports: [
